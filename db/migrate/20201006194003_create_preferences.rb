56bd9906db7114115adbe37f2e76d73867503723
Samantha Aleman <samanthaaleman@Samanthas-MacBook-Air.local>
2020-10-06 12:48:29 -0700

    Done.

---
+++ db/migrate/20201006194003_create_preferences.rb
@@ -0,0 +1,12 @@
+class CreatePreferences < ActiveRecord::Migration[5.0]
+  def change
+    create_table :preferences do |t|
+      t.datetime :created_at
+      t.datetime :updated_at
+      t.boolean :allow_create_songs
+      t.boolean :allow_create_artists
+
+      t.timestamps
+    end
+  end
+end
